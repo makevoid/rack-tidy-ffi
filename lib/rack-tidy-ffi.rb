9c1190ef1ec0248b810c52d7678ec7b7a36f2cec
Francesco Canessa <makevoid@gmail.com>
2012-04-10 02:13:10 +0200

    added rails support

--- lib/rack-tidy-ffi.rb
+++ lib/rack-tidy-ffi.rb
@@ -3,23 +3,27 @@ require "#{path}/tidy-machine"
 
 class RackTidyFFI
   attr_accessor :headers
-  
+
   def initialize(app)
-    @app = app       
-  end             
-  
-  def call(env)   
+    @app = app
+  end
+
+  def call(env)
     @status, @headers, @response = @app.call(env)
-    # TODO: include all response attributes... needed?
-    # response_body = ""
-    # response_body = @response.each { |part| response_body += part }
     if @headers["Content-Type"].include? "text/html"
-      response = TidyMachine.new(@response.first).tidy
+      body = if @response.is_a? Array
+        @response.first
+      elsif @response.is_a? Rack::BodyProxy
+        @response.body.first
+      else
+        @response.body
+      end
+      response = TidyMachine.new(body).tidy
       response = "<!DOCTYPE HTML>\n#{response}"
       @headers["Content-Length"] = response.length.to_s
       @response = [response]
     end
     [@status, @headers, @response]
-  end            
-  
+  end
+
 end
